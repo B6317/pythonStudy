277e82b92181e6f002930a771fba4ffacda4f589
李太新 <litaixin@donews.com>
2018-01-29 09:52:48 +0800

    将图片调整到 iphone5的尺寸以下

--- study-1/test_5.py
+++ study-1/test_5.py
@@ -1,2 +1,25 @@
 # coding=utf-8
 # create by toonew at 2018/1/28
+from __future__ import print_function
+
+import random
+import string
+from os import listdir
+from os.path import isfile, join
+
+from PIL import ImageDraw, Image
+
+mypath = '/Users/toonew/test'
+
+onlyfiles = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
+
+
+def change_size(img_path):
+    img = Image.open(img_path)
+    resized = img.resize((640, 1136), resample=Image.LANCZOS)
+    resized.save("./test/result" + "".join(random.sample(string.digits, 3)) + '.jpg', format="jpeg")
+
+
+print(onlyfiles)
+for imgPath in onlyfiles:
+    change_size(imgPath)
